f47c01a7f591979d5c3844f8bc6d0ec5a2343552
Di213 <139703406+Di213@users.noreply.github.com>
2024-02-05 18:51:21 +0000

    update

--- Projeto_UFCD5412/Projeto_UFCD5412/Form1.Designer.cs
+++ Projeto_UFCD5412/Projeto_UFCD5412/Form1.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.joao = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // joao
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(535, 78);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.joao.AutoSize = true;
+            this.joao.Location = new System.Drawing.Point(535, 78);
+            this.joao.Name = "joao";
+            this.joao.Size = new System.Drawing.Size(30, 13);
+            this.joao.TabIndex = 0;
+            this.joao.Text = "Joao";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.joao);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +55,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label joao;
     }
 }
 

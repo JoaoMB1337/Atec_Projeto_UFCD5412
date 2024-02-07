1df5314284639d83ecaf38b095a2d2f2069cda5e
Diogo Sousa <diogosousainf@gmail.com>
2024-02-07 18:45:40 +0000

    updateLayoutFuncionario
    ismael da merge

--- Projeto_UFCD5412/Projeto_UFCD5412/View/FuncionarioForms/AdicionarFuncionarioForm.Designer.cs
+++ Projeto_UFCD5412/Projeto_UFCD5412/View/FuncionarioForms/AdicionarFuncionarioForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdicionarFuncionarioForm));
             this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
             this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
             this.labelIDFuncionario = new System.Windows.Forms.Label();
@@ -60,10 +61,11 @@
             this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.None;
             this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
             this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconPictureBox1.IconSize = 100;
+            this.iconPictureBox1.IconSize = 123;
             this.iconPictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.iconPictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.iconPictureBox1.Name = "iconPictureBox1";
-            this.iconPictureBox1.Size = new System.Drawing.Size(800, 100);
+            this.iconPictureBox1.Size = new System.Drawing.Size(1067, 123);
             this.iconPictureBox1.TabIndex = 0;
             this.iconPictureBox1.TabStop = false;
             // 
@@ -75,165 +77,184 @@
             this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.None;
             this.iconPictureBox2.IconColor = System.Drawing.SystemColors.ControlText;
             this.iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconPictureBox2.IconSize = 350;
-            this.iconPictureBox2.Location = new System.Drawing.Point(0, 100);
+            this.iconPictureBox2.IconSize = 431;
+            this.iconPictureBox2.Location = new System.Drawing.Point(0, 123);
+            this.iconPictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.iconPictureBox2.Name = "iconPictureBox2";
-            this.iconPictureBox2.Size = new System.Drawing.Size(800, 350);
+            this.iconPictureBox2.Size = new System.Drawing.Size(1067, 431);
             this.iconPictureBox2.TabIndex = 1;
             this.iconPictureBox2.TabStop = false;
             // 
             // labelIDFuncionario
             // 
             this.labelIDFuncionario.AutoSize = true;
-            this.labelIDFuncionario.Location = new System.Drawing.Point(37, 129);
+            this.labelIDFuncionario.Location = new System.Drawing.Point(49, 159);
+            this.labelIDFuncionario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelIDFuncionario.Name = "labelIDFuncionario";
-            this.labelIDFuncionario.Size = new System.Drawing.Size(21, 13);
+            this.labelIDFuncionario.Size = new System.Drawing.Size(23, 16);
             this.labelIDFuncionario.TabIndex = 2;
             this.labelIDFuncionario.Text = "ID:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(64, 126);
+            this.textBox1.Location = new System.Drawing.Point(85, 155);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 3;
             // 
             // NomeFunc_Label
             // 
             this.NomeFunc_Label.AutoSize = true;
-            this.NomeFunc_Label.Location = new System.Drawing.Point(37, 163);
+            this.NomeFunc_Label.Location = new System.Drawing.Point(49, 201);
+            this.NomeFunc_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.NomeFunc_Label.Name = "NomeFunc_Label";
-            this.NomeFunc_Label.Size = new System.Drawing.Size(38, 13);
+            this.NomeFunc_Label.Size = new System.Drawing.Size(47, 16);
             this.NomeFunc_Label.TabIndex = 4;
             this.NomeFunc_Label.Text = "Nome:";
             // 
             // MoradaFunc_Label
             // 
             this.MoradaFunc_Label.AutoSize = true;
-            this.MoradaFunc_Label.Location = new System.Drawing.Point(37, 196);
+            this.MoradaFunc_Label.Location = new System.Drawing.Point(49, 241);
+            this.MoradaFunc_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.MoradaFunc_Label.Name = "MoradaFunc_Label";
-            this.MoradaFunc_Label.Size = new System.Drawing.Size(46, 13);
+            this.MoradaFunc_Label.Size = new System.Drawing.Size(57, 16);
             this.MoradaFunc_Label.TabIndex = 5;
             this.MoradaFunc_Label.Text = "Morada:";
             // 
             // ContactoFunc_Label
             // 
             this.ContactoFunc_Label.AutoSize = true;
-            this.ContactoFunc_Label.Location = new System.Drawing.Point(37, 222);
+            this.ContactoFunc_Label.Location = new System.Drawing.Point(49, 273);
+            this.ContactoFunc_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ContactoFunc_Label.Name = "ContactoFunc_Label";
-            this.ContactoFunc_Label.Size = new System.Drawing.Size(53, 13);
+            this.ContactoFunc_Label.Size = new System.Drawing.Size(63, 16);
             this.ContactoFunc_Label.TabIndex = 6;
             this.ContactoFunc_Label.Text = "Contacto:";
             // 
             // TipoFunc_Label
             // 
             this.TipoFunc_Label.AutoSize = true;
-            this.TipoFunc_Label.Location = new System.Drawing.Point(37, 248);
+            this.TipoFunc_Label.Location = new System.Drawing.Point(49, 305);
+            this.TipoFunc_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TipoFunc_Label.Name = "TipoFunc_Label";
-            this.TipoFunc_Label.Size = new System.Drawing.Size(31, 13);
+            this.TipoFunc_Label.Size = new System.Drawing.Size(38, 16);
             this.TipoFunc_Label.TabIndex = 7;
             this.TipoFunc_Label.Text = "Tipo:";
             // 
             // DataContrato_Label
             // 
             this.DataContrato_Label.AutoSize = true;
-            this.DataContrato_Label.Location = new System.Drawing.Point(37, 275);
+            this.DataContrato_Label.Location = new System.Drawing.Point(49, 338);
+            this.DataContrato_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.DataContrato_Label.Name = "DataContrato_Label";
-            this.DataContrato_Label.Size = new System.Drawing.Size(90, 13);
+            this.DataContrato_Label.Size = new System.Drawing.Size(109, 16);
             this.DataContrato_Label.TabIndex = 8;
             this.DataContrato_Label.Text = "Data do contrato:";
             // 
             // FimContrato_Label
             // 
             this.FimContrato_Label.AutoSize = true;
-            this.FimContrato_Label.Location = new System.Drawing.Point(37, 302);
+            this.FimContrato_Label.Location = new System.Drawing.Point(49, 372);
+            this.FimContrato_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.FimContrato_Label.Name = "FimContrato_Label";
-            this.FimContrato_Label.Size = new System.Drawing.Size(121, 13);
+            this.FimContrato_Label.Size = new System.Drawing.Size(148, 16);
             this.FimContrato_Label.TabIndex = 9;
             this.FimContrato_Label.Text = "Data do fim de contrato:";
             // 
             // DataRegistoCriminal_Label
             // 
             this.DataRegistoCriminal_Label.AutoSize = true;
-            this.DataRegistoCriminal_Label.Location = new System.Drawing.Point(37, 331);
+            this.DataRegistoCriminal_Label.Location = new System.Drawing.Point(49, 407);
+            this.DataRegistoCriminal_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.DataRegistoCriminal_Label.Name = "DataRegistoCriminal_Label";
-            this.DataRegistoCriminal_Label.Size = new System.Drawing.Size(111, 13);
+            this.DataRegistoCriminal_Label.Size = new System.Drawing.Size(140, 16);
             this.DataRegistoCriminal_Label.TabIndex = 10;
             this.DataRegistoCriminal_Label.Text = "Data Registo Criminal:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(81, 160);
+            this.textBox2.Location = new System.Drawing.Point(108, 197);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 11;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(89, 193);
+            this.textBox3.Location = new System.Drawing.Point(119, 238);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
             this.textBox3.TabIndex = 12;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(96, 219);
+            this.textBox4.Location = new System.Drawing.Point(128, 270);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(132, 22);
             this.textBox4.TabIndex = 13;
             // 
             // TipoFuncionario_ComboBox
             // 
             this.TipoFuncionario_ComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.TipoFuncionario_ComboBox.FormattingEnabled = true;
-            this.TipoFuncionario_ComboBox.Location = new System.Drawing.Point(81, 245);
+            this.TipoFuncionario_ComboBox.Location = new System.Drawing.Point(108, 302);
+            this.TipoFuncionario_ComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TipoFuncionario_ComboBox.Name = "TipoFuncionario_ComboBox";
-            this.TipoFuncionario_ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.TipoFuncionario_ComboBox.Size = new System.Drawing.Size(160, 24);
             this.TipoFuncionario_ComboBox.TabIndex = 14;
             this.TipoFuncionario_ComboBox.SelectedIndexChanged += new System.EventHandler(this.TipoFuncionario_ComboBox_SelectedIndexChanged);
             // 
             // DataContrato_DateTimePicker
             // 
-            this.DataContrato_DateTimePicker.Location = new System.Drawing.Point(133, 272);
+            this.DataContrato_DateTimePicker.Location = new System.Drawing.Point(177, 335);
+            this.DataContrato_DateTimePicker.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DataContrato_DateTimePicker.Name = "DataContrato_DateTimePicker";
-            this.DataContrato_DateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.DataContrato_DateTimePicker.Size = new System.Drawing.Size(265, 22);
             this.DataContrato_DateTimePicker.TabIndex = 15;
             // 
             // DataFimContrato_DateTimePicker
             // 
-            this.DataFimContrato_DateTimePicker.Location = new System.Drawing.Point(173, 302);
+            this.DataFimContrato_DateTimePicker.Location = new System.Drawing.Point(231, 372);
+            this.DataFimContrato_DateTimePicker.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DataFimContrato_DateTimePicker.Name = "DataFimContrato_DateTimePicker";
-            this.DataFimContrato_DateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.DataFimContrato_DateTimePicker.Size = new System.Drawing.Size(265, 22);
             this.DataFimContrato_DateTimePicker.TabIndex = 16;
             // 
             // DataRegistoCriminal_DateTimePicker
             // 
-            this.DataRegistoCriminal_DateTimePicker.Location = new System.Drawing.Point(173, 331);
+            this.DataRegistoCriminal_DateTimePicker.Location = new System.Drawing.Point(231, 407);
+            this.DataRegistoCriminal_DateTimePicker.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DataRegistoCriminal_DateTimePicker.Name = "DataRegistoCriminal_DateTimePicker";
-            this.DataRegistoCriminal_DateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.DataRegistoCriminal_DateTimePicker.Size = new System.Drawing.Size(265, 22);
             this.DataRegistoCriminal_DateTimePicker.TabIndex = 17;
             // 
             // DataFimRegistoCriminal_Label
             // 
             this.DataFimRegistoCriminal_Label.AutoSize = true;
-            this.DataFimRegistoCriminal_Label.Location = new System.Drawing.Point(37, 361);
+            this.DataFimRegistoCriminal_Label.Location = new System.Drawing.Point(49, 444);
+            this.DataFimRegistoCriminal_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.DataFimRegistoCriminal_Label.Name = "DataFimRegistoCriminal_Label";
-            this.DataFimRegistoCriminal_Label.Size = new System.Drawing.Size(130, 13);
+            this.DataFimRegistoCriminal_Label.Size = new System.Drawing.Size(165, 16);
             this.DataFimRegistoCriminal_Label.TabIndex = 18;
             this.DataFimRegistoCriminal_Label.Text = "Data Fim Registo Criminal:";
             // 
             // DataFimRegistoCriminal_DateTimePicker
             // 
-            this.DataFimRegistoCriminal_DateTimePicker.Location = new System.Drawing.Point(173, 361);
+            this.DataFimRegistoCriminal_DateTimePicker.Location = new System.Drawing.Point(231, 444);
+            this.DataFimRegistoCriminal_DateTimePicker.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DataFimRegistoCriminal_DateTimePicker.Name = "DataFimRegistoCriminal_DateTimePicker";
-            this.DataFimRegistoCriminal_DateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.DataFimRegistoCriminal_DateTimePicker.Size = new System.Drawing.Size(265, 22);
             this.DataFimRegistoCriminal_DateTimePicker.TabIndex = 19;
             // 
             // AdicionarFuncionarioForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.DataFimRegistoCriminal_DateTimePicker);
             this.Controls.Add(this.DataFimRegistoCriminal_Label);
             this.Controls.Add(this.DataRegistoCriminal_DateTimePicker);
@@ -254,8 +275,11 @@
             this.Controls.Add(this.labelIDFuncionario);
             this.Controls.Add(this.iconPictureBox2);
             this.Controls.Add(this.iconPictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AdicionarFuncionarioForm";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Adicionar funcionario";
             this.Load += new System.EventHandler(this.AdicionarFuncionarioForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();

a89e07e8c7789e6490a795049ddd17184ea806bf
Di213 <139703406+Di213@users.noreply.github.com>
2024-02-07 01:23:08 +0000

    Main
    - Fiz uma side bar
    - Fiz alguns metedos no main para o form

--- Projeto_UFCD5412/Projeto_UFCD5412/View/MainWindow.Designer.cs
+++ Projeto_UFCD5412/Projeto_UFCD5412/View/MainWindow.Designer.cs
@@ -28,23 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.ExportarFicheiro_Btn = new FontAwesome.Sharp.IconButton();
+            this.CalcularValorPagar_Btn = new FontAwesome.Sharp.IconButton();
+            this.VerRegistoCriminal_Btn = new FontAwesome.Sharp.IconButton();
+            this.VerFuncionarioContrato_Btn = new FontAwesome.Sharp.IconButton();
+            this.AlterarRegisto_Btn = new FontAwesome.Sharp.IconButton();
+            this.AddFuncionario_Btn = new FontAwesome.Sharp.IconButton();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.Home_Btn = new System.Windows.Forms.PictureBox();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Home_Btn)).BeginInit();
             this.SuspendLayout();
-
-
             // 
-            // Form1
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panelMenu.Controls.Add(this.ExportarFicheiro_Btn);
+            this.panelMenu.Controls.Add(this.CalcularValorPagar_Btn);
+            this.panelMenu.Controls.Add(this.VerRegistoCriminal_Btn);
+            this.panelMenu.Controls.Add(this.VerFuncionarioContrato_Btn);
+            this.panelMenu.Controls.Add(this.AlterarRegisto_Btn);
+            this.panelMenu.Controls.Add(this.AddFuncionario_Btn);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 605);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // ExportarFicheiro_Btn
+            // 
+            this.ExportarFicheiro_Btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ExportarFicheiro_Btn.FlatAppearance.BorderSize = 0;
+            this.ExportarFicheiro_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExportarFicheiro_Btn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.ExportarFicheiro_Btn.IconChar = FontAwesome.Sharp.IconChar.FloppyDisk;
+            this.ExportarFicheiro_Btn.IconColor = System.Drawing.Color.Gainsboro;
+            this.ExportarFicheiro_Btn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.ExportarFicheiro_Btn.IconSize = 32;
+            this.ExportarFicheiro_Btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ExportarFicheiro_Btn.Location = new System.Drawing.Point(0, 440);
+            this.ExportarFicheiro_Btn.Name = "ExportarFicheiro_Btn";
+            this.ExportarFicheiro_Btn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.ExportarFicheiro_Btn.Size = new System.Drawing.Size(220, 60);
+            this.ExportarFicheiro_Btn.TabIndex = 5;
+            this.ExportarFicheiro_Btn.Text = "ExportarFicheiro_Btn";
+            this.ExportarFicheiro_Btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ExportarFicheiro_Btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.ExportarFicheiro_Btn.UseVisualStyleBackColor = true;
+            this.ExportarFicheiro_Btn.Click += new System.EventHandler(this.ExportarFicheiro_Btn_Click);
+            // 
+            // CalcularValorPagar_Btn
+            // 
+            this.CalcularValorPagar_Btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.CalcularValorPagar_Btn.FlatAppearance.BorderSize = 0;
+            this.CalcularValorPagar_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CalcularValorPagar_Btn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.CalcularValorPagar_Btn.IconChar = FontAwesome.Sharp.IconChar.MoneyBill;
+            this.CalcularValorPagar_Btn.IconColor = System.Drawing.Color.Gainsboro;
+            this.CalcularValorPagar_Btn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.CalcularValorPagar_Btn.IconSize = 32;
+            this.CalcularValorPagar_Btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.CalcularValorPagar_Btn.Location = new System.Drawing.Point(0, 380);
+            this.CalcularValorPagar_Btn.Name = "CalcularValorPagar_Btn";
+            this.CalcularValorPagar_Btn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.CalcularValorPagar_Btn.Size = new System.Drawing.Size(220, 60);
+            this.CalcularValorPagar_Btn.TabIndex = 4;
+            this.CalcularValorPagar_Btn.Text = "CalcularValorPagar_Btn";
+            this.CalcularValorPagar_Btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.CalcularValorPagar_Btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.CalcularValorPagar_Btn.UseVisualStyleBackColor = true;
+            this.CalcularValorPagar_Btn.Click += new System.EventHandler(this.CalcularValorPagar_Btn_Click);
+            // 
+            // VerRegistoCriminal_Btn
+            // 
+            this.VerRegistoCriminal_Btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.VerRegistoCriminal_Btn.FlatAppearance.BorderSize = 0;
+            this.VerRegistoCriminal_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.VerRegistoCriminal_Btn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.VerRegistoCriminal_Btn.IconChar = FontAwesome.Sharp.IconChar.Navicon;
+            this.VerRegistoCriminal_Btn.IconColor = System.Drawing.Color.Gainsboro;
+            this.VerRegistoCriminal_Btn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.VerRegistoCriminal_Btn.IconSize = 32;
+            this.VerRegistoCriminal_Btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.VerRegistoCriminal_Btn.Location = new System.Drawing.Point(0, 320);
+            this.VerRegistoCriminal_Btn.Name = "VerRegistoCriminal_Btn";
+            this.VerRegistoCriminal_Btn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.VerRegistoCriminal_Btn.Size = new System.Drawing.Size(220, 60);
+            this.VerRegistoCriminal_Btn.TabIndex = 3;
+            this.VerRegistoCriminal_Btn.Text = "VerRegistoCriminal_Btn";
+            this.VerRegistoCriminal_Btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.VerRegistoCriminal_Btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.VerRegistoCriminal_Btn.UseVisualStyleBackColor = true;
+            this.VerRegistoCriminal_Btn.Click += new System.EventHandler(this.VerRegistoCriminal_Btn_Click);
+            // 
+            // VerFuncionarioContrato_Btn
+            // 
+            this.VerFuncionarioContrato_Btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.VerFuncionarioContrato_Btn.FlatAppearance.BorderSize = 0;
+            this.VerFuncionarioContrato_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.VerFuncionarioContrato_Btn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.VerFuncionarioContrato_Btn.IconChar = FontAwesome.Sharp.IconChar.Table;
+            this.VerFuncionarioContrato_Btn.IconColor = System.Drawing.Color.Gainsboro;
+            this.VerFuncionarioContrato_Btn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.VerFuncionarioContrato_Btn.IconSize = 32;
+            this.VerFuncionarioContrato_Btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.VerFuncionarioContrato_Btn.Location = new System.Drawing.Point(0, 260);
+            this.VerFuncionarioContrato_Btn.Name = "VerFuncionarioContrato_Btn";
+            this.VerFuncionarioContrato_Btn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.VerFuncionarioContrato_Btn.Size = new System.Drawing.Size(220, 60);
+            this.VerFuncionarioContrato_Btn.TabIndex = 2;
+            this.VerFuncionarioContrato_Btn.Text = "VerFuncionarioContrato_Btn";
+            this.VerFuncionarioContrato_Btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.VerFuncionarioContrato_Btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.VerFuncionarioContrato_Btn.UseVisualStyleBackColor = true;
+            this.VerFuncionarioContrato_Btn.Click += new System.EventHandler(this.VerFuncionarioContrato_Btn_Click);
+            // 
+            // AlterarRegisto_Btn
+            // 
+            this.AlterarRegisto_Btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AlterarRegisto_Btn.FlatAppearance.BorderSize = 0;
+            this.AlterarRegisto_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AlterarRegisto_Btn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.AlterarRegisto_Btn.IconChar = FontAwesome.Sharp.IconChar.Book;
+            this.AlterarRegisto_Btn.IconColor = System.Drawing.Color.Gainsboro;
+            this.AlterarRegisto_Btn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.AlterarRegisto_Btn.IconSize = 32;
+            this.AlterarRegisto_Btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AlterarRegisto_Btn.Location = new System.Drawing.Point(0, 200);
+            this.AlterarRegisto_Btn.Name = "AlterarRegisto_Btn";
+            this.AlterarRegisto_Btn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.AlterarRegisto_Btn.Size = new System.Drawing.Size(220, 60);
+            this.AlterarRegisto_Btn.TabIndex = 1;
+            this.AlterarRegisto_Btn.Text = "AlterarRegisto_Btn";
+            this.AlterarRegisto_Btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AlterarRegisto_Btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.AlterarRegisto_Btn.UseVisualStyleBackColor = true;
+            this.AlterarRegisto_Btn.Click += new System.EventHandler(this.AlterarRegisto_Btn_Click);
+            // 
+            // AddFuncionario_Btn
+            // 
+            this.AddFuncionario_Btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AddFuncionario_Btn.FlatAppearance.BorderSize = 0;
+            this.AddFuncionario_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddFuncionario_Btn.ForeColor = System.Drawing.Color.Gainsboro;
+            this.AddFuncionario_Btn.IconChar = FontAwesome.Sharp.IconChar.UserTie;
+            this.AddFuncionario_Btn.IconColor = System.Drawing.Color.Gainsboro;
+            this.AddFuncionario_Btn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.AddFuncionario_Btn.IconSize = 32;
+            this.AddFuncionario_Btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AddFuncionario_Btn.Location = new System.Drawing.Point(0, 140);
+            this.AddFuncionario_Btn.Name = "AddFuncionario_Btn";
+            this.AddFuncionario_Btn.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.AddFuncionario_Btn.Size = new System.Drawing.Size(220, 60);
+            this.AddFuncionario_Btn.TabIndex = 0;
+            this.AddFuncionario_Btn.Text = "AddFuncionario_Btn";
+            this.AddFuncionario_Btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AddFuncionario_Btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.AddFuncionario_Btn.UseVisualStyleBackColor = true;
+            this.AddFuncionario_Btn.Click += new System.EventHandler(this.AddFuncionario_Btn_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.Home_Btn);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 140);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // Home_Btn
+            // 
+            this.Home_Btn.Image = ((System.Drawing.Image)(resources.GetObject("Home_Btn.Image")));
+            this.Home_Btn.Location = new System.Drawing.Point(52, 12);
+            this.Home_Btn.Name = "Home_Btn";
+            this.Home_Btn.Size = new System.Drawing.Size(113, 112);
+            this.Home_Btn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Home_Btn.TabIndex = 0;
+            this.Home_Btn.TabStop = false;
+            this.Home_Btn.Click += new System.EventHandler(this.Home_Btn_Click);
+            // 
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(875, 605);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "MainWindow";
             this.Text = "Form1";
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Home_Btn)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Panel panelMenu;
+        private FontAwesome.Sharp.IconButton AddFuncionario_Btn;
+        private System.Windows.Forms.Panel panelLogo;
+        private FontAwesome.Sharp.IconButton ExportarFicheiro_Btn;
+        private FontAwesome.Sharp.IconButton CalcularValorPagar_Btn;
+        private FontAwesome.Sharp.IconButton VerRegistoCriminal_Btn;
+        private FontAwesome.Sharp.IconButton VerFuncionarioContrato_Btn;
+        private FontAwesome.Sharp.IconButton AlterarRegisto_Btn;
+        private System.Windows.Forms.PictureBox Home_Btn;
     }
 }
 
--- Projeto_UFCD5412/Projeto_UFCD5412/View/MainWindow.cs
+++ Projeto_UFCD5412/Projeto_UFCD5412/View/MainWindow.cs
@@ -7,14 +7,105 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FontAwesome.Sharp;
 
 namespace Projeto_UFCD5412
 {
     public partial class MainWindow : Form
-    {
+    {   
+        //Variáveis
+        private IconButton currentBtn;
+        private Panel leftBorderBtn;
         public MainWindow()
         {
             InitializeComponent();
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(7, 60);
+            panelMenu.Controls.Add(leftBorderBtn);
+        }
+        //Eventos
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(172, 126, 241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+            public static Color color4 = Color.FromArgb(95, 77, 221);
+            public static Color color5 = Color.FromArgb(249, 88, 155);          
+            public static Color color6 = Color.FromArgb(24, 161, 251);          
+        }                                                                       
+                                                                                
+        //Métodos                                                               
+        private void ActivateButton(object senderBtn, Color color)
+        {
+            if (senderBtn != null)
+            {
+                DisableButton();
+                currentBtn = (IconButton)senderBtn;
+                currentBtn.BackColor = Color.FromArgb(37, 36, 81);
+                currentBtn.ForeColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor = color;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentBtn.ImageAlign = ContentAlignment.MiddleRight;
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location = new Point(0, currentBtn.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+            }
+        }
+
+        private void DisableButton()
+        {
+            if (currentBtn != null)
+            {
+                currentBtn.BackColor = Color.FromArgb(31, 30, 68);
+                currentBtn.ForeColor = Color.Gainsboro;
+                currentBtn.TextAlign = ContentAlignment.MiddleLeft;
+                currentBtn.IconColor = Color.Gainsboro;
+                currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void AddFuncionario_Btn_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color1);
+        }
+
+        private void AlterarRegisto_Btn_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color2);
+        }
+
+        private void VerFuncionarioContrato_Btn_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color3);
+        }
+
+        private void VerRegistoCriminal_Btn_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color4);
+        }
+
+        private void CalcularValorPagar_Btn_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color5);
+        }
+
+        private void ExportarFicheiro_Btn_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color6);
+        }
+
+        private void Home_Btn_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            DisableButton();
+            leftBorderBtn.Visible = false;
         }
     }
 }
